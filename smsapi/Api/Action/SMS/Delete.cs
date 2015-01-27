fee31e44af8f086814e61a27143448af36de26a8
Marek Sudoł <m.sudol@smsapi.pl>
2015-01-27 13:38:54 +0100

    remove unusable method

--- smsapi/Api/Action/SMS/Delete.cs
+++ smsapi/Api/Action/SMS/Delete.cs
@@ -8,7 +8,7 @@ namespace SMSApi.Api.Action
 
         protected override string Uri() { return "sms.do"; }
 
-        protected string[] ids;
+        protected string id;
 
         protected override NameValueCollection Values()
         {
@@ -19,21 +19,14 @@ namespace SMSApi.Api.Action
             collection.Add("username", client.GetUsername());
             collection.Add("password", client.GetPassword());
 
-            collection.Add("sch_del", string.Join("|", ids));
+            collection.Add("sch_del", id);
 
             return collection;
         }
 
         public SMSDelete Id(string id)
         {
-            this.ids = new string[] { id };
-            return this;
-        }
-
-
-        public SMSDelete Id(string[] ids)
-        {
-            this.ids = ids;
+            this.id = new string { id };
             return this;
         }
     }
