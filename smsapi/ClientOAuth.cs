463dc54137a55e63735f34621bfc51311da06897
knowak <takczyowakkamilnowak@gmail.com>
2020-02-01 21:45:54 +0100

    ClientOAuth added

---
+++ smsapi/ClientOAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace SMSApi.Api
+{
+    public class ClientOAuth : IClient
+    {
+        public string Token { get; }
+
+        public ClientOAuth(string token)
+        {
+            Token = token;
+        }
+
+        public string GetAuthenticationHeader()
+        {
+            return "Bearer " + Convert.ToBase64String(Encoding.UTF8.GetBytes(Token));
+        }
+    }
+}
