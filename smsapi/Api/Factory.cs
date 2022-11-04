3fcb52178a9cde9cae5fbbd0e0f67c65a5eed922
knowak <takczyowakkamilnowak@gmail.com>
2022-11-04 02:23:11 +0100

    Add SmsApiIo proxy

--- smsapi/Api/Factory.cs
+++ smsapi/Api/Factory.cs
@@ -1,18 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace SMSApi.Api
+﻿namespace SMSApi.Api
 {
     public abstract class Factory
     {
-        private static Dictionary<ProxyAddress, string> _addresses =
-            new Dictionary<ProxyAddress, string>
-            {
-                { ProxyAddress.SmsApiPl, "https://api.smsapi.pl/" },
-                { ProxyAddress.BackupSmsApiPl, "https://api2.smsapi.pl/" },
-                { ProxyAddress.SmsApiCom, "https://api.smsapi.com/" },
-                { ProxyAddress.BackupSmsApiCom, "https://api2.smsapi.com/" }
-            };
-
         protected Proxy proxy;
 
         private IClient client;
@@ -48,7 +37,7 @@ namespace SMSApi.Api
 
         private void Proxy(ProxyAddress address)
         {
-            Proxy(new ProxyHTTP(_addresses[address]));
+            Proxy(new ProxyHTTP(address.GetUrl()));
         }
     }
 }
--- smsapi/ProxyAddress.cs
+++ smsapi/ProxyAddress.cs
@@ -2,9 +2,36 @@
 {
     public enum ProxyAddress
     {
+        SmsApiIo,
         SmsApiPl,
         BackupSmsApiPl,
         SmsApiCom,
         BackupSmsApiCom
+    }
+
+    public static class ProxyAddressExtensions
+    {
+        public static string GetUrl(this ProxyAddress proxy)
+        {
+            switch (proxy)
+            {
+                case ProxyAddress.SmsApiIo:
+                    return "https://smsapi.io/";
+
+                case ProxyAddress.SmsApiPl:
+                    return "https://api.smsapi.pl/";
+
+                case ProxyAddress.BackupSmsApiPl:
+                    return "https://api2.smsapi.pl/";
+
+                case ProxyAddress.SmsApiCom:
+                    return "https://api.smsapi.com/";
+
+                case ProxyAddress.BackupSmsApiCom:
+                    return "https://api2.smsapi.com/";
+            }
+
+            throw new ProxyException("Proxy address does not exist.");
+        }
     }
 }
