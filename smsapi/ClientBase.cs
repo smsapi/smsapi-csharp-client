3bd85e785c0a26c8faac514a3ec25fea3e3b4781
knowak <takczyowakkamilnowak@gmail.com>
2022-11-13 19:07:24 +0100

    Update user agent header

--- smsapi/ClientBase.cs
+++ smsapi/ClientBase.cs
@@ -6,18 +6,11 @@ namespace SMSApi.Api
 {
     public abstract class ClientBase
     {
-        private string _clientAgent;
+        private readonly string _clientAgent;
 
         public ClientBase()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-
-            _clientAgent = string.Format(
-                "smsapi/csharp-client:{0};assemblyFile:{1};.net:{2}",
-                assembly.GetName().Version,
-                fvi.FileVersion,
-                Environment.Version);
+            _clientAgent = $"smsapi-csharp-client:{Assembly.GetExecutingAssembly().GetName().Version};{Environment.Version}";
         }
 
         public abstract string GetAuthenticationHeader();
