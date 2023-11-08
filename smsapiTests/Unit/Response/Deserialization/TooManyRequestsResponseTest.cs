b73d4c1e166caa29e8a7a66be36a293828f0d3dc
jlabno <kwas@outlook.com>
2023-11-08 18:26:39 +0100

    Handle standard REST http codes

---
+++ smsapiTests/Unit/Response/Deserialization/TooManyRequestsResponseTest.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SMSApi.Api;
+using SMSApi.Api.Action;
+using SMSApi.Api.Response.Deserialization;
+using SMSApi.Api.Response.ResponseResolver;
+using smsapiTests.Unit.Fixture;
+using smsapiTests.Unit.Helper;
+
+namespace smsapiTests.Unit.Response.Deserialization;
+
+[TestClass]
+public class TooManyRequestsResponseTest
+{
+    private readonly ProxyStub _proxyStub = new();
+
+    [TestMethod]
+    public void map_validation_errors_to_message()
+    {
+        var action = new TestAction();
+        action.Proxy(_proxyStub);
+        Dictionary<string, dynamic> response = new();
+        _proxyStub.SyncExecutionResponse = new HttpResponseEntity(
+            response.ToHttpEntityStreamTask(),
+            HttpStatusCode.TooManyRequests
+        );
+
+        var execution = () => action.Execute();
+        
+        Assert.ThrowsException<TooManyRequestsErrorResolver.TooManyRequestsException>(execution);
+    }
+
+    private class TestAction : Base<ResponseWithExceptionMapper>
+    {
+        protected override RequestMethod Method { get; }
+
+        protected override ApiType ApiType()
+        {
+            return SMSApi.Api.Action.ApiType.Rest;
+        }
+
+        protected override string Uri()
+        {
+            return "";
+        }
+    }
+
+    private class ResponseWithExceptionMapper : IResponseCodeAwareResolver
+    {
+    }
+}
