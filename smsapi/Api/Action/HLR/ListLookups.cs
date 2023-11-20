8b63ee05026e24e77455e6a8a9932297cfca9b70
jakublabno <kwas@outlook.com>
2023-11-20 13:03:22 +0100

    HLR feature

--- smsapi/Api/Action/HLR/ListLookups.cs
+++ smsapi/Api/Action/HLR/ListLookups.cs
@@ -1,14 +1,17 @@
 using SMSApi.Api.Response;
 using SMSApi.Api.Response.HLR;
+using SMSApi.Api.Response.ResponseResolver;
 
 namespace SMSApi.Api.Action;
 
-public class ListLookups : Action<BasicCollection<LookupResult>>, IPaginable
+public class ListLookups : Action<BasicCollection<LookupResult>>, IPaginable, IResponseCodeAwareResolver
 {
     protected override RequestMethod Method => RequestMethod.GET;
     
     protected override string Uri() => "hlr/lookups";
-    
+
+    protected override ApiType ApiType() => Action.ApiType.Rest;
+
     public uint? Limit { get; set; }
     public uint? Offset { get; set; }
 }
