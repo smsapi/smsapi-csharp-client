d9b22835600113d5106549621c657a47c813b0c7
jlabno <kwas@outlook.com>
2024-05-24 09:43:41 +0200

    Allow using own HttpClient in default proxy

--- smsapi/Api/Features.cs
+++ smsapi/Api/Features.cs
@@ -12,13 +12,19 @@ public class Features
         Proxy = new ProxyHTTP(proxy.GetUrl());
         Client = client;
     }
-    
+
     public Features(IClient client, HttpClient httpClient)
     {
         Proxy = new ProxyHTTP(ProxyAddress.SmsApiIo.GetUrl(), httpClient);
         Client = client;
     }
-    
+
+    public Features(IClient client, HttpClient httpClient, ProxyAddress proxy = ProxyAddress.SmsApiIo)
+    {
+        Proxy = new ProxyHTTP(proxy.GetUrl(), httpClient);
+        Client = client;
+    }
+
     public Features(IClient client, Proxy proxy)
     {
         Proxy = proxy;
