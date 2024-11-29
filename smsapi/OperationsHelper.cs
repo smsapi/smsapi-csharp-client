6922bc21e3df627c77e7fabaceaa14eea5174d74
Jakub Łabno <jakub.labno@linkmobility.com>
2024-11-29 10:12:02 +0000

    OperationsHelper visibility

--- smsapi/OperationsHelper.cs
+++ smsapi/OperationsHelper.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace SMSApi.Api;
 
-public static class OperationsHelper
+internal static class OperationsHelper
 {
     public static void Let<T>(this T value, Action<T> action) where T : class
     {
