4da21444b151ae1ea7b24d4597b2826a54d9283f
Jakub Łabno <jakub.labno@linkmobility.com>
2025-01-02 09:06:57 +0000

    Revert "Ease pagination usage"
    This reverts commit 24ceeeac5144369090811630322451036287506b.

--- examples/Pagination.cs
+++ examples/Pagination.cs
@@ -3,11 +3,29 @@ using SMSApi.Api;
 var client = new ClientOAuth("token");
 var features = new Features(client);
 
-var list = features.Sendernames()
-    .List()
-    .ToIterator();
-
-foreach (var sendername in list)
+var listSendernames = (uint collectionLimit, uint collectionOffset) =>
 {
-    Console.WriteLine(sendername.Sender);
-}
+    var list = features.Sendernames().List();
+
+    list.Limit = collectionLimit;
+    list.Offset = collectionOffset;
+
+    return list.Execute();
+};
+
+const uint limit = 25;
+uint offset = 0;
+bool hasMoreItems;
+
+do
+{
+    var sendernames = listSendernames(limit, offset);
+
+    sendernames.Collection.ForEach(sendername =>
+    {
+        Console.WriteLine($"Sender: {sendername.Sender}");
+    });
+
+    hasMoreItems = sendernames.Size > limit + offset;
+    offset += limit;
+} while (hasMoreItems);
--- smsapi/Api/Response/Deserialization/LegacyJsonResponseDeserializer.cs
+++ smsapi/Api/Response/Deserialization/LegacyJsonResponseDeserializer.cs
@@ -30,6 +30,10 @@ namespace SMSApi.Api.Response.Deserialization
             {
                 throw new HostException(e.Message, HostException.E_JSON_DECODE);
             }
+            finally
+            {
+                data?.Close();
+            }
 
             return response;
         }
